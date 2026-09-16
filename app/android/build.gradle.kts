4a141473f1d9affce4e37f980b4d87d239c03ebc
anglesgirl <anglesgirl@github.com>
2026-09-16 07:17:41 +0000

    ech: 独立包名+桌面标记，构建注入统计钥匙

--- app/android/build.gradle.kts
+++ app/android/build.gradle.kts
@@ -37,7 +37,7 @@ android {
     namespace = "me.him188.ani.android"
     compileSdk = getIntProperty("android.compile.sdk")
     defaultConfig {
-        applicationId = "me.him188.ani"
+        applicationId = "me.him188.ani.ech"
         minSdk = getIntProperty("android.min.sdk")
         targetSdk = getIntProperty("android.compile.sdk")
         versionCode = getIntProperty("android.version.code")
